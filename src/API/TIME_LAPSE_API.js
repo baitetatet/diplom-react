2c9404e5e853a8ca320124aae800a87b165d8ee4
baitetatet <di.baitov@mail.ru>
2023-01-20 23:02:42 +0300

    [A] Add WEEK_DAYS_API and TIME_LAPSE_API

---
+++ src/API/TIME_LAPSE_API.js
@@ -0,0 +1,6 @@
+export const TIME_LAPSE = [
+  { id: 'timeId_6', timeValue: '06:00' }, { id: 'timeId_7', timeValue: '07:00' }, { id: 'timeId_8', timeValue: '08:00' }, { id: 'timeId_9', timeValue: '09:00' },
+  { id: 'timeId_10', timeValue: '10:00' }, { id: 'timeId_11', timeValue: '11:00' }, { id: 'timeId_12', timeValue: '12:00' }, { id: 'timeId_13', timeValue: '13:00' },
+  { id: 'timeId_14', timeValue: '14:00' }, { id: 'timeId_15', timeValue: '15:00' }, { id: 'timeId_16', timeValue: '16:00' }, { id: 'timeId_17', timeValue: '17:00' },
+  { id: 'timeId_18', timeValue: '18:00' }, { id: 'timeId_19', timeValue: '19:00' }
+]
---
+++ src/API/WEEK_DAYS_API.js
@@ -0,0 +1,37 @@
+export const WEEK_DAYS = [
+  {
+    id: '1',
+    title: 'Понедельник',
+    shortTitle: 'Пн'
+  },
+  {
+    id: '2',
+    title: 'Вторник',
+    shortTitle: 'Вт'
+  },
+  {
+    id: '3',
+    title: 'Среда',
+    shortTitle: 'Ср'
+  },
+  {
+    id: '4',
+    title: 'Четверг',
+    shortTitle: 'Чт'
+  },
+  {
+    id: '5',
+    title: 'Пятница',
+    shortTitle: 'Пт'
+  },
+  {
+    id: '6',
+    title: 'Суббота',
+    shortTitle: 'Сб'
+  },
+  {
+    id: '7',
+    title: 'Воскресенье',
+    shortTitle: 'Вс'
+  }
+]
