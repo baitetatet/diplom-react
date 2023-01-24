36e3cca7d89192869f609bcb0abba480ba163daf
baitetatet <di.baitov@mail.ru>
2023-01-24 22:54:42 +0300

    [A] Add ConfirmationTasks component

---
+++ src/Components/ConfirmationTasks/ConfirmationTask/ConfirmationTask.jsx
@@ -0,0 +1,44 @@
+import avatarSRC from '../../../images/avatar.png'
+
+export const ConfirmationTask = () => {
+  const VARIABLES = {
+    avatarSRC: avatarSRC,
+    userName: 'п-к Петров А.',
+    timeComplete: 'Сегодня в 11:38',
+    downLoadButton: 'Скачать файл',
+    confirmButton: 'Подтвердить',
+    contentTask: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ad repudiandae, aut, accusantium perferendis quod autem laudantium natus, illum tenetur placeat unde porro explicabo modi praesentium qui sed pariatur nemo?'
+  }
+
+
+  return (
+    <article className="confirmation-task">
+      <div className="confirmation-task__inner">
+        <div className="confirmation-task__header">
+          <div className="confirmation-task__user-info">
+            <div className="confirmation-task__user-info_avatar">
+              <img src={VARIABLES.avatarSRC} alt="avatar" />
+            </div>
+            <div className="confirmation-task__user-info_name">
+              {VARIABLES.userName}
+            </div>
+          </div>
+          <div className="confirmation-task__time-complete">
+            {VARIABLES.timeComplete}
+          </div>
+        </div>
+        <div className="confirmation-task__content">
+          {VARIABLES.contentTask}
+        </div>
+        <div className="confirmation-task__buttons">
+          <button className="confirmation-task__buttons_download-file button-grey">
+            {VARIABLES.downLoadButton}
+          </button>
+          <button className="confirmation-task__buttons_confirm button-grey">
+            {VARIABLES.confirmButton}
+          </button>
+        </div>
+      </div>
+    </article>
+  )
+}
--- src/Components/ConfirmationTasks/ConfirmationTasks.jsx
+++ src/Components/ConfirmationTasks/ConfirmationTasks.jsx
@@ -1,7 +1,32 @@
+import { ConfirmationTask } from "./ConfirmationTask/ConfirmationTask"
+
 export const ConfirmationTasks = () => {
+  const VARIABLES = {
+    componentTitle: 'Задачи на подтверждение'
+  }
+
   return (
     <section className="confirmation-tasks">
-      <h2>TASK ON CONFIRM</h2>
+      <div className="confirmation-tasks__inner">
+        <div className="confirmation-tasks__header">
+          <h2 className="confirmation-tasks__header_title">
+            {VARIABLES.componentTitle}
+          </h2>
+        </div>
+        <div className="confirmation-tasks__content">
+          <ConfirmationTask />
+          <ConfirmationTask />
+          <ConfirmationTask />
+          <ConfirmationTask />
+          <ConfirmationTask />
+          <ConfirmationTask />
+          <ConfirmationTask />
+          <ConfirmationTask />
+          <ConfirmationTask />
+
+          <ConfirmationTask />
+        </div>
+      </div>
     </section>
   )
 }
