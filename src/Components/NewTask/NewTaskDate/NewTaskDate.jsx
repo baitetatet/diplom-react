75af0e5791ab63314939d6b4d340ff0230c5fee9
baitetatet <di.baitov@mail.ru>
2023-02-24 14:23:08 +0300

    [A] Add date new task

---
+++ src/Components/NewTask/NewTaskDate/NewTaskDate.jsx
@@ -0,0 +1,28 @@
+export const NewTaskDate = () => {
+  const VARIABLES = {
+    title: 'Период проведения: ',
+    labelDateStart: 'c',
+    labelDateEnd: 'до'
+  }
+  return (
+    <div className="new-task__date">
+      <h3 className='new-task__date_title'>{VARIABLES.title}</h3>
+      <label htmlFor='dateStart'>
+        {VARIABLES.labelDateStart}
+        <input
+          name="dateStart"
+          id="dateStart"
+          type='date'
+        />
+      </label>
+      <label htmlFor='dateEnd'>
+        {VARIABLES.labelDateEnd}
+        <input
+          name="dateEnd"
+          id="dateEnd"
+          type='date'
+        />
+      </label>
+    </div>
+  )
+}
